235628b7c7cd44c24094ac0d8c71b4c0aa141d07
sandynguyen8928 <sandynguyen8928@gmail.com>
2023-12-20 18:59:21 -0500

    added animation on hover for name

--- src/pages/contact/contact.tsx
+++ src/pages/contact/contact.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import styles from "./contact.module.scss";
 import github_logo from "../../assets/github.png";
 import linkedin_logo from "../../assets/linkedin.png"
+import {motion} from "framer-motion";
 
 const contact = () => {
     return (
         <div className={styles["contact-styled"]}>
-            <div className={styles["name"]}>studio</div>
-            <div className={styles["name"]}>sandy</div>
+            <motion.div className={styles["name"]} whileHover={{ scale: 2.0 }} transition={{ type: "spring", damping: 10 }}>studio <br/>sandy</motion.div>
             <div className={styles["email"]}>sandycatnguyen@gmail.com</div>
             <div className={styles["socials"]}>
                 <a href="https://github.com/sandynguyen8928" target="_blank" rel="noopener noreferrer"><img src={github_logo} className={styles["social-logo"]} alt="github logo"/></a>
