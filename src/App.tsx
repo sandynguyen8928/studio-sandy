4747f7deb0dc6f1ad562c1482fd79a90787fb443
sandynguyen8928 <sandynguyen8928@gmail.com>
2023-12-25 19:46:00 -0500

    added a horizontal scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import styles from "./App.module.scss";
 import Landing from "./pages/landing/landing";
 import Projects from "./pages/projects/projects";
 import Contact from "./pages/contact/contact";
 
 function App() {
+
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    const handleScroll = (event: React.WheelEvent<HTMLDivElement>) => {
+        if (containerRef.current) {
+            containerRef.current.scrollLeft += event.deltaY;
+            event.preventDefault();
+        }
+    };
+
     return (
-        <div className={styles["App"]}>
+        <div className={styles["App"]} onWheel={handleScroll} ref={containerRef}>
             <Landing />
             <Projects />
             <Contact />
